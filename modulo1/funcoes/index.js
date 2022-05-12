0a63f445b56ee2fa67b531b1a613372e4d59a9a1
savio henriques mourao ayres <savioayres@hotmail.com>
2022-05-12 18:20:45 -0300

    Exercício de funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,94 @@
+// Exercício de interpretação de código
+
+// Questão 1
+
+// 10, 50  Nesse caso foi feita uma multiplicação com a variável
+//  e usando "minhaFunção"
+
+// Questão 2
+
+// a function "minhaFuncao" não teria uma função
+
+// Questão 3
+
+// A)
+// Essa função irá trazer todo o texto em letra minúcula
+// e verificar se a palavra cenoura é true ou false dependendo
+// do texto escrito
+
+// B)true
+// true
+// true
+
+
+// Exercícios de escrita de código
+
+// Questão 1
+
+// const info1 = "Sávio"
+// const info2 = 26
+// const info3 = "Campina Grande"
+// const info4 = "estudante"
+// // const frase = {`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao`}
+
+// const texto = (info1, info2, info3, info4) => info1 + info2 + info3 + info4
+// const textoCompleto = (`Eu sou ${info1}, tenho ${info2} anos, moro em ${info3} e sou ${info4}`)
+
+// console.log(textoCompleto)
+
+
+// Questão 2
+
+// A)let numero1 = 10
+// let numero2 = 20
+
+// function soma(numero1, numero2) {
+// return numero1 + numero2
+// }
+
+// console.log(soma(numero1, numero2))
+
+// LETRA C
+
+// const numero1 = 10
+// const numero2 = 20
+
+// function somar(a, b){ 
+
+// somar= (a % b) === 0
+// return somar
+
+// }
+
+// console.log(somar(numero1, numero2))
+
+// Questão B
+
+// const numero1 = 10
+// const numero2 = 20
+
+// function somar(a, b){ 
+// somar = a>b
+
+// return somar
+
+// }
+
+// console.log(somar(numero1, numero2))
+
+// Questão D
+
+// let mensagem = "meu nome é sávio"
+
+// function palavra(nome){
+// nome = nome.toUpperCase()
+// let tamanho = nome.length
+// return[nome, tamanho]
+
+// }
+// const resultado = palavra(mensagem)
+// console.log(resultado)
+
+// Questão 3
+
+let operacoes =
