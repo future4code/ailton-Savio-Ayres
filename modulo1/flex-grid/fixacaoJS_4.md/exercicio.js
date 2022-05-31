b63aaf588b964bc56f50b4e8369f086c2584034d
savio henriques mourao ayres <savioayres@hotmail.com>
2022-05-30 23:17:32 -0300

    Exercício de Javascript

---
+++ modulo1/flex-grid/fixacaoJS_4.md/exercicio.js
@@ -0,0 +1,15 @@
+///function contaOcorrencias(arrayDeNumeros, numeroEscolhido) {
+    let contador = 0
+    for(let numero of arrayDeNumeros) {
+      if (numero === numeroEscolhido) {
+        contador ++
+      }
+      
+    }
+    if (contador === 0 ) {
+      return "Número não encontrado"
+    }else{
+      return `O número ${numeroEscolhido} aparece ${contador}x`
+    }
+    
+    ///
