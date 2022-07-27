52140fa506a78b92da4d056ec32f64e5bddd373b
savio henriques mourao ayres <savioayres@hotmail.com>
2022-07-27 17:41:03 -0300

    Implementando o React Router

--- modulo3/cookenu/src/App.js
+++ modulo3/cookenu/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Router from './routes/Router'
 
 const App = () => {
-  return <div>Cookenu</div>
+  return (
+    <div>
+      <Router />
+    </div>
+  )
 }
 
 export default App
---
+++ modulo3/cookenu/src/pages/RecipesListPage/RecipesListPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const RecipesListPage = () => {
+  return (
+    <di>
+      <h1>RecipesListPage</h1>
+    </di>
+  )
+}
+export default RecipesListPage
